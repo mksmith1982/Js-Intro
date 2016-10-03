ed045f5b914e33c43efdea7de191dd5bc51d9698
mksmith1982 <mksmith1982@gmail.com>
2016-10-03 19:49:59 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,64 @@
+/*=========================================================
+    Part 1: Variables
+*/
+var givenName;
+// Q: What does `givenName` equal right now?
+// A:
+
+
+givenName = "Brit";
+// Q: What is `givenName` set to right now?
+// A:
+
+
+givenName = givenName;
+// Q: What is `givenName` set to right now?
+// A:
+
+
+var greeting = "Hello, how are you? " + givenName;
+// Q: What is `greeting` set to?
+// A:
+
+
+/*=========================================================
+     Part 2: Simple Math
+*/
+
+var high = 50;
+var low  = 10;
+
+
+var math = high - low;
+// Q: What is `math` set to?
+// A:
+
+math = high - "5";
+// Q: What is `math` set to?
+// A:
+
+
+/* =========================================================
+     Part 3: Expressions - You Write Code!
+*/
+
+// Create a variable to calculate Brit's age
+// The answer should read "Brit is 33 years old"
+// The answer should not be written in a comment.
+var born = 1983;
+var today = 2016;
+
+// Answers Below:
+
+
+
+// Store some information following in variables.
+var yourName;
+var instructorName;
+
+// Update the variables above so the expression reads correctly.
+// Answers Below:
+
+
+// Final Statement
+var statement = yourName + " is taking a class at The Iron Yard. Their instructor's name is " + instructorName;
